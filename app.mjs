9ca9eacd316d189a62221ac33c5312ee6336c25f
Emrul Islam <emrul@emrul.com>
2022-10-27 00:38:49 +0100

    Doc update

--- app.mjs
+++ app.mjs
@@ -113,11 +113,11 @@ async function initApp(app) {
             // Make sure user is allowed to access profile
             if ( !userGroupNames.includes(requestedProfile.applicableToGroup) ) throw new Error(`User '${userEmail}' has no access to profile '${requestedProfileName}'`);
 
-            // Make sure user is allowed to access select group
+            // Make sure user is allowed to access selected group
             if ( !requestedProfile.groups.includes(selectedGroup) ) throw new Error(`User '${userEmail}' not allowed to access requested group '${selectedGroup}' in profile '${requestedProfileName}'`);
 
             if ( typeof profileConfig.groupPermissions[selectedGroup] === "string") {
-                // Switching to this group requires that user is already in another group
+                // Switching to this group requires that user is already in another group, exit if they don't have permission
                 if ( !userGroupNames.includes(profileConfig.groupPermissions[selectedGroup])) throw new Error(`User '${userEmail}' has no access to group '${selectedGroup}' in profile '${requestedProfileName}' because they are not a member of required group '${profileConfig.groupPermissions[selectedGroup]}'`)
             }
 
