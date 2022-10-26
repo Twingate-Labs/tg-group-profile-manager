7cc62bd1002daee8ed84ab1f4e6ead984e64efb0
chenbishop <chen.bishop@gmail.com>
2022-10-26 15:51:18 +0100

    typo

--- appHome.mjs
+++ appHome.mjs
@@ -16,7 +16,7 @@ export const createHome = async(profileConfig, userEmail) => {
                      type: "section",
                      text: {
                          type: "mrkdwn",
-                         text: "*Welcome!* \nUsing this tool you can change your group profiles.**Please note all changes can take at least 20 seconds to propagate."
+                         text: "*Welcome!* \nUsing this tool you can change your group profiles.\n*Please note all changes can typically take at least 20 seconds to propagate.*"
                      }
                     },
                      {
