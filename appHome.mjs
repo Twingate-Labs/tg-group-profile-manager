65d94519e83e86346865a461364cb42494426a35
chenbishop <chen.bishop@gmail.com>
2022-10-26 17:14:52 +0100

    improved crete home code readibility

--- appHome.mjs
+++ appHome.mjs
@@ -7,48 +7,10 @@ export const createHome = async(profileConfig, userEmail) => {
         const profileManager = new SlackProfileManager()
         await profileManager.init()
         const userWithGroups = await profileManager.lookupUserGroupByEmail(userEmail);
-        if (!userWithGroups) {
-            console.log(`Email '${userEmail}' not found in Twingate`)
-             return {
-                 "type": "home",
-                 "blocks": [
-                     {
-                     type: "section",
-                     text: {
-                         type: "mrkdwn",
-                         text: "*Welcome!* \nUsing this tool you can change your group profiles.\n*Please note all changes can typically take at least 20 seconds to propagate.*"
-                     }
-                    },
-                     {
-                         type: "context",
-                         elements: [
-                             {
-                                 type: "mrkdwn",
-                                 text: "<https://github.com/Twingate-Labs/tg-group-profile-manager|GitHub> and <https://github.com/Twingate-Labs/tg-group-profile-manager/blob/main/README.md|User Guide>"
-                             }
-                         ]
-                     },
-                     {
-                         type: "divider"
-                     },
-                     {
-                         type: "section",
-                         "text": {
-                             "type": "mrkdwn",
-                             "text": `*ERROR*\n Email '${userEmail}' not found in Twingate, please ensure your Slack account email address is the same as your Twingate email address.`
-                         }
-                     }
-                 ]
-             }
-        }
-
-        const userId = userWithGroups.id
-        const userGroups = userWithGroups.groups.edges.map(group => group.node)
-        const permittedProfiles = profileConfig.profiles.filter(profile => userGroups.map(group=>group.name).includes(profile.applicableToGroup))
-
-        let view = {
-                "type": "home",
-                "blocks": [{
+        const view = {
+            "type": "home",
+            "blocks": [
+                {
                     type: "section",
                     text: {
                         type: "mrkdwn",
@@ -64,10 +26,26 @@ export const createHome = async(profileConfig, userEmail) => {
                         }
                     ]
                 }
-                ]
+            ]
+        }
+        if (!userWithGroups) {
+            console.log(`Email '${userEmail}' not found in Twingate`)
+            view.blocks.push({
+                    type: "divider"
+                },
+                {
+                    type: "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": `*ERROR*\n Email '${userEmail}' not found in Twingate, please ensure your Slack account email address is the same as your Twingate email address.`
+                    }
+                })
+             return view
         }
 
-        const splitter = {type: "divider"}
+        const userId = userWithGroups.id
+        const userGroups = userWithGroups.groups.edges.map(group => group.node)
+        const permittedProfiles = profileConfig.profiles.filter(profile => userGroups.map(group=>group.name).includes(profile.applicableToGroup))
 
         for (const permittedProfile of permittedProfiles){
             const currentActiveGroups = permittedProfile.groups.filter(group => userGroups.map(userGroup => userGroup.name).includes(group))
@@ -92,8 +70,7 @@ export const createHome = async(profileConfig, userEmail) => {
                     "value": `${permittedProfile.profileName}++${userEmail}`
                 }
             }
-            view.blocks.push(splitter)
-            view.blocks.push(block)
+            view.blocks.push({type: "divider"}, block)
         }
         return view
     }
